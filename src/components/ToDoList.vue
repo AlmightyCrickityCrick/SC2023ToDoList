5509ed191a27c049d54be98becde45f89a245a20
Lina <seong92@mail.ru>
2023-07-13 15:47:52 +0300

    Data  and computed interpolation

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,13 +1,19 @@
 <template>
 <h1>My To Do List</h1>
-
+<p>{{ this.prettyGreeting }} </p>
 </template>
 
 <script>
 export default{
-    data(){
+    data: function(){
         return{
+            greeting: "Salutare si bine ai venit la Lista Mea",
             
+        }
+    },
+    computed: {
+        prettyGreeting: function(){
+            return this.greeting + "✨"
         }
     }
     
